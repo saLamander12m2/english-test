4677d659f1a71aa018f8dcda25337128aa3c48ff
saLamander12m2 <michaelvl2009.123@gmail.com>
2023-09-17 15:29:58 +0300

    feat: create project

---
+++ src/main/java/ru/school_activity/english_test/EnglishTestApplication.java
@@ -0,0 +1,13 @@
+package ru.school_activity.english_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTestApplication.class, args);
+    }
+
+}
