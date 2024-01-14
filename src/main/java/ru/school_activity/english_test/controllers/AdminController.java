9653b10fb71f82cda5573f73f655601afafc2d92
saLamander12m2 <michaelvl2009.123@gmail.com>
2024-01-14 20:53:19 +0300

    feat: create error message for upload page

--- src/main/java/ru/school_activity/english_test/controllers/AdminController.java
+++ src/main/java/ru/school_activity/english_test/controllers/AdminController.java
@@ -3,9 +3,7 @@ package ru.school_activity.english_test.controllers;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,19 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import ru.school_activity.english_test.entity.Answer;
-import ru.school_activity.english_test.entity.TestQuestion;
-import ru.school_activity.english_test.entity.TopicVerb;
-import ru.school_activity.english_test.entity.WrongAnswer;
-import ru.school_activity.english_test.service.AnswerService;
-import ru.school_activity.english_test.service.TestQuestionService;
-import ru.school_activity.english_test.service.TopicVerbService;
-import ru.school_activity.english_test.service.WrongAnswerService;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import ru.school_activity.english_test.service.UploadService;
 
 @Controller
 @RequiredArgsConstructor
@@ -36,10 +22,7 @@ import java.util.List;
 @RequestMapping(value = "/admin")
 public class AdminController {
 
-    private final TopicVerbService topicVerbService;
-    private final TestQuestionService testQuestionService;
-    private final AnswerService answerService;
-    private final WrongAnswerService wrongAnswerService;
+    private final UploadService uploadService;
 
     @Secured("ROLE_ADMIN")
     @GetMapping()
@@ -49,63 +32,18 @@ public class AdminController {
         model.addAttribute("isAuthenticated", isAuthenticated);
         model.addAttribute("name", httpSession.getAttribute("name"));
 
+        String result = (String) httpSession.getAttribute("result");
+        model.addAttribute("result", result);
+        httpSession.removeAttribute("result");
         return "admin";
     }
 
     @Secured("ROLE_ADMIN")
     @PostMapping(value = "/upload")
-    public String upload(@RequestParam("file") MultipartFile multipartFile) {
+    public String upload(@RequestParam("file") MultipartFile multipartFile, HttpSession httpSession) {
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(multipartFile.getInputStream()))) {
-
-            String line = reader.readLine();
-            int i = 0;
-
-            while ((line = reader.readLine()) != null) {
-
-                String[] stringArray = line.split(";");
-
-                if (stringArray.length == 7){
-                    System.out.println(Arrays.toString(stringArray));
-                    i++;
-
-
-                    TopicVerb topicVerb;
-                    topicVerb = topicVerbService.getTopicVerbByVerb(stringArray[1].toLowerCase());
-                    if (topicVerb == null) {
-                        topicVerb = new TopicVerb(stringArray[1].toLowerCase());
-                        topicVerbService.save(topicVerb);
-                    }
-
-                    TestQuestion testQuestion = new TestQuestion(stringArray[2]);
-                    testQuestion.setTopicVerb(topicVerb);
-                    testQuestionService.save(testQuestion);
-
-                    try {
-                        Answer answer = new Answer(stringArray[3].toLowerCase(), testQuestion);
-
-                        List<WrongAnswer> wrongAnswers = new ArrayList<>();
-                        for (int y = 4; y < 7; y++) {
-                            wrongAnswers.add(new WrongAnswer(stringArray[y].toLowerCase(), testQuestion));
-                        }
-
-                        answerService.save(answer);
-                        wrongAnswers.forEach(wrongAnswerService::save);
-
-                        log.info("Successfully save testQuestion -> {}", testQuestion.toString());
-                    } catch (DataIntegrityViolationException e) {
-                        log.info("TestQuestion already exists -> {}", testQuestion.toString());
-                    }
-                }
-            }
-            log.info("Всего в файле {} строк", i);
-
-            return "redirect:/admin";
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "admin";
+        String result = uploadService.uploadFile(multipartFile);
+        httpSession.setAttribute("result", result);
+        return "redirect:/admin";
     }
 }
---
+++ src/main/java/ru/school_activity/english_test/service/UploadService.java
@@ -0,0 +1,85 @@
+package ru.school_activity.english_test.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.school_activity.english_test.entity.Answer;
+import ru.school_activity.english_test.entity.TestQuestion;
+import ru.school_activity.english_test.entity.TopicVerb;
+import ru.school_activity.english_test.entity.WrongAnswer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UploadService {
+
+    private final TopicVerbService topicVerbService;
+    private final TestQuestionService testQuestionService;
+    private final AnswerService answerService;
+    private final WrongAnswerService wrongAnswerService;
+
+    public String uploadFile(MultipartFile multipartFile) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(multipartFile.getInputStream()))) {
+
+            String line = reader.readLine();
+            int successLine = 0;
+            int totalLine = 0;
+
+            while ((line = reader.readLine()) != null) {
+                totalLine++;
+
+                String[] stringArray = line.split(";");
+
+                if (stringArray.length == 7) {
+
+                    TopicVerb topicVerb;
+                    topicVerb = topicVerbService.getTopicVerbByVerb(stringArray[1].toLowerCase());
+                    if (topicVerb == null) {
+                        topicVerb = new TopicVerb(stringArray[1].toLowerCase());
+                        topicVerbService.save(topicVerb);
+                    }
+
+                    TestQuestion testQuestion = new TestQuestion(stringArray[2]);
+                    testQuestion.setTopicVerb(topicVerb);
+                    try {
+                        testQuestionService.save(testQuestion);
+
+                        Answer answer = new Answer(stringArray[3].toLowerCase(), testQuestion);
+
+                        List<WrongAnswer> wrongAnswers = new ArrayList<>();
+                        for (int y = 4; y < 7; y++) {
+                            wrongAnswers.add(new WrongAnswer(stringArray[y].toLowerCase(), testQuestion));
+                        }
+
+                        answerService.save(answer);
+                        wrongAnswers.forEach(wrongAnswerService::save);
+
+                        log.info("Successfully save testQuestion -> {}", testQuestion.toString());
+                        successLine++;
+                    } catch (DataIntegrityViolationException e) {
+                        log.info("TestQuestion already exists -> {}", testQuestion.toString());
+                        stringBuilder.append("- Error in line %d: question for test already exists<br>".formatted(totalLine));
+                    }
+                } else {
+                    stringBuilder.append("- Error in line %d: there are not enough arguments for the question<br>".formatted(totalLine));
+                }
+            }
+            log.info("Всего в файле {} строк", totalLine);
+            stringBuilder.append("Successfully upload %d from %d lines".formatted(successLine, totalLine));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info(stringBuilder.toString());
+        return stringBuilder.toString();
+    }
+}
